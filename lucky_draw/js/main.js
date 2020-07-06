c9cb1385ebf7b15696e172a16ec9725b8becf299
Bill Tao <billtao@pku.edu.cn>
2020-07-06 17:46:41 +0800

    Bug fix

--- lucky_draw/js/main.js
+++ lucky_draw/js/main.js
@@ -1,12 +1,10 @@
 const canvas = $("#canvas")[0];
 const ctx = canvas.getContext('2d');
 const board = $("#board_image")[0];
-canvas.style.height = canvas.style.width;
 canvas.height = canvas.width = 1000;
 let theta = 0, omega = 0, alpha = -0.3, delta_t = 0.02, status = 0, interval = null, repeated = false, result = 0;
-const button = $("#button-draw")[0];
 const back_threshold = -Math.PI / 3
-canvas.fillStyle = "rgb(206,119,84)"
+ctx.fillStyle = "rgb(163,85,213)"
 const names = ["手链", "猫爪钥匙扣", "手帐", "谢谢惠顾", "香水", "唇膏", "月球灯"];
 const img_name = ["bracelet", "key", "agenda", "", "perfume", "lipstick", "lamp"];
 
@@ -40,27 +38,25 @@ function update() {
 
 function reset() {
     status = theta = omega = 0;
-    $("#button-draw").addClass("btn btn-block btn-danger").removeClass("disabled btn-success").removeAttr("disabled")
+    $("#button-draw").addClass("btn btn-block btn-danger").removeClass("disabled btn-success").removeAttr("disabled").text("开始")
     update();
 }
 
 reset();
-button.onmousedown = function () {
+$("#button-draw").bind("mousedown touchstart", function () {
     if (status !== 0)
         return;
     status = 1;
     omega = -0.5;
     interval = setInterval(update, delta_t * 1000)
-    button.classList.remove('btn-danger')
-    button.classList.add("btn-success")
-}
-button.onmouseup = function () {
+    $("#button-draw").toggleClass("btn-danger btn-warning").text("蓄力中...")
+}).bind("mouseup touchend", function () {
     if (status !== 1)
         return
     status = 2;
     omega = theta * -10 + Math.random() * 5;
-    $("#button-draw").attr("disabled", "")
-}
+    $("#button-draw").attr("disabled", "").text("抽奖中...").toggleClass("btn-warning btn-success")
+})
 
 function check_item() {
     theta = theta * 180 / Math.PI;
@@ -82,8 +78,9 @@ function postprocess() {
     let item = check_item();
     if (item.id === 3) {
         if (repeated) {
-            $("#dialog-content").text("emmmmm.....我应该把的謝意送给谁呢?")
+            $("#dialog-content").text("emmmmm.....我应该把我的謝意送给谁呢?")
             $("#dialog-title").text("抽到了\"Thank you\"!")
+            $("#draw-name").text("谢谢惠顾")
             $("#dialog-normal").modal({
                 backdrop: 'static',
                 keyboard: false
@@ -93,17 +90,19 @@ function postprocess() {
         $("#dialog-thankyou").modal('show');
         return
     }
+    $("#draw-name").text(item.name)
     $("#dialog-content").text("emmmmm.....我应该把这" + item.name + "送给谁呢?")
     $("#dialog-normal").modal({
         backdrop: 'static',
-        keyboard: false
+        keyboard: false,
+        show: true
     })
     result = item.id;
 }
 
 function confirm() {
     let name = $("#name-input")[0].value;
-    $("#name-confirm").removeClass("btn-primary").addClass("btn-secondary").attr("disabled","").text("请稍后....")
+    $("#name-confirm").removeClass("btn-primary").addClass("btn-secondary").attr("disabled", "").text("请稍后....")
     fetch("https://b1dca5512b98ce4f6a6cd2a9ad9c34fa.m.pipedream.net", {
         method: "POST",
         // mode: "no-cors",
@@ -111,7 +110,10 @@ function confirm() {
             name: name,
             id: result
         })
-    }).then((response=>response.json())).then(data=>console.log(data));
+    }).then((response => response.json())).then(function (data) {
+        if (data.success)
+            $("#name-confirm").text("完成!").removeClass("btn-secondary").addClass("btn-success")
+    });
 }
 
 $("#dialog-thankyou").on("hidden.bs.modal", function () {
